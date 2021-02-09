aef953c1967050c00c2478f4660252b33aee15c8
Alec Gunny <alec.gunny@gmail.com>
2021-02-09 10:22:00 -0800

    adding channels to separate file

--- client.sh
+++ client.sh
@@ -1,7 +1,26 @@
 #! /bin/bash -e
 
-
 IMAGE='deepclean-prod:client-20.07'
+CHANNELS=$(cat channels.txt)
+DATA_DIR='/dev/shm/llhoft/H1'
+FILE_PATTERN='H-H1_llhoft-{}-1.gwf'
+CMD="
+    python client.py
+        --url 34.82.145.3
+            --model-name gwe2e
+            --model-version 1
+            --sequence-id 1001
+            --kernel-stride 0.002
+            --witness-h-data-dir $DATA_DIR
+            --witness-l-data-dir $DATA_DIR
+            --strain-data-dir $DATA_DIR
+            --witness-h-file-pattern $FILE_PATTERN
+            --witness-l-file-pattern $FILE_PATTERN
+            --strain-file-pattern $FILE_PATTERN
+            --witness-h-channels $CHANNELS
+            --witness-l-channels $CHANNELS
+            --strain-channels $CHANNELS
+"
 singularity exec \
     --home $PWD:/srv \
     --pwd /srv \
@@ -11,62 +30,4 @@ singularity exec \
     --scratch /tmp \
     --pid \
     /cvmfs/singularity.opensciencegrid.org/alec.gunny/$IMAGE \
-        python client.py \
-            --url 34.82.145.3 \
-            --model-name gwe2e \
-            --model-version 1 \
-            --sequence-id 1001 \
-            --kernel-stride 0.002 \
-            --witness-h-data-dir "/dev/shm/llhoft/H1" \
-            --witness-l-data-dir "/dev/shm/llhoft/H1" \
-            --strain-data-dir "/dev/shm/llhoft/H1" \
-            --witness-h-file-pattern "H-H1_llhoft-{}-1.gwf" \
-            --witness-l-file-pattern "H-H1_llhoft-{}-1.gwf" \
-            --strain-file-pattern "H-H1_llhoft-{}-1.gwf" \
-            --witness-h-channels \
-                H1:PEM-CS_MAINSMON_EBAY_1_DQ \
-                H1:ASC-INP1_P_INMON \
-                H1:ASC-INP1_Y_INMON \
-                H1:ASC-MICH_P_INMON \
-                H1:ASC-MICH_Y_INMON \
-                H1:ASC-PRC1_P_INMON \
-                H1:ASC-PRC1_Y_INMON \
-                H1:ASC-PRC2_P_INMON \
-                H1:ASC-PRC2_Y_INMON \
-                H1:ASC-SRC1_P_INMON \
-                H1:ASC-SRC1_Y_INMON \
-                H1:ASC-SRC2_P_INMON \
-                H1:ASC-SRC2_Y_INMON \
-                H1:ASC-DHARD_P_INMON \
-                H1:ASC-DHARD_Y_INMON \
-                H1:ASC-CHARD_P_INMON \
-                H1:ASC-CHARD_Y_INMON \
-                H1:ASC-DSOFT_P_INMON \
-                H1:ASC-DSOFT_Y_INMON \
-                H1:ASC-CSOFT_P_INMON \
-                H1:ASC-CSOFT_Y_INMON \
-            --witness-l-channels \
-                H1:PEM-CS_MAINSMON_EBAY_1_DQ \
-                H1:ASC-INP1_P_INMON \
-                H1:ASC-INP1_Y_INMON \
-                H1:ASC-MICH_P_INMON \
-                H1:ASC-MICH_Y_INMON \
-                H1:ASC-PRC1_P_INMON \
-                H1:ASC-PRC1_Y_INMON \
-                H1:ASC-PRC2_P_INMON \
-                H1:ASC-PRC2_Y_INMON \
-                H1:ASC-SRC1_P_INMON \
-                H1:ASC-SRC1_Y_INMON \
-                H1:ASC-SRC2_P_INMON \
-                H1:ASC-SRC2_Y_INMON \
-                H1:ASC-DHARD_P_INMON \
-                H1:ASC-DHARD_Y_INMON \
-                H1:ASC-CHARD_P_INMON \
-                H1:ASC-CHARD_Y_INMON \
-                H1:ASC-DSOFT_P_INMON \
-                H1:ASC-DSOFT_Y_INMON \
-                H1:ASC-CSOFT_P_INMON \
-                H1:ASC-CSOFT_Y_INMON \
-            --strain-channels \
-                H1:GDS-CALIB_STRAIN \
-                H1:PEM-CS_MAINSMON_EBAY_1_DQ \
+        /bin/bash -c "source activate deepclean && $CMD"
