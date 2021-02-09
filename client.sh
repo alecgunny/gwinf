fc665667a205bcb1e2a48a2aee6234e9dbd001c1
Alec L Gunny <alec.gunny@ldas-pcdev11.ligo.caltech.edu>
2021-02-09 10:45:54 -0800

    fixing stillwater type checks

--- client.sh
+++ client.sh
@@ -1,26 +1,29 @@
 #! /bin/bash -e
 
 IMAGE='deepclean-prod:client-20.07'
-CHANNELS=$(cat channels.txt)
+CHANNELS=( $(cat channels.txt) )
 DATA_DIR='/dev/shm/llhoft/H1'
 FILE_PATTERN='H-H1_llhoft-{}-1.gwf'
 CMD="
-    python client.py
-        --url 34.82.145.3
-            --model-name gwe2e
-            --model-version 1
-            --sequence-id 1001
-            --kernel-stride 0.002
-            --witness-h-data-dir $DATA_DIR
-            --witness-l-data-dir $DATA_DIR
-            --strain-data-dir $DATA_DIR
-            --witness-h-file-pattern $FILE_PATTERN
-            --witness-l-file-pattern $FILE_PATTERN
-            --strain-file-pattern $FILE_PATTERN
-            --witness-h-channels $CHANNELS
-            --witness-l-channels $CHANNELS
-            --strain-channels $CHANNELS
+    python client.py \
+        --url 34.82.145.3 \
+            --model-name gwe2e \
+            --model-version 1 \
+            --sequence-id 1001 \
+            --kernel-stride 0.002 \
+            --witness-h-data-dir $DATA_DIR \
+            --witness-l-data-dir $DATA_DIR \
+            --strain-data-dir $DATA_DIR \
+            --witness-h-file-pattern $FILE_PATTERN \
+            --witness-l-file-pattern $FILE_PATTERN \
+            --strain-file-pattern $FILE_PATTERN \
+            --witness-h-channels ${CHANNELS[@]:1} \
+            --witness-l-channels ${CHANNELS[@]:1} \
+            --strain-channels ${CHANNELS[@]:0:2} \
 "
+echo "Running command:"
+echo $CMD
+
 singularity exec \
     --home $PWD:/srv \
     --pwd /srv \
