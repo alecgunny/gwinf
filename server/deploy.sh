e90203f64aa2699e22ec613042b61cd9fec54165
Alec Gunny <alec.gunny@gmail.com>
2021-02-23 21:53:37 -0800

    fixing helm issues

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -e
+GPUS=4
 gcloud container node-pools create big-t4-pool \
     --cluster gw-triton-dev \
     --machine-type=n1-standard-8 \
-    --accelerator=type=nvidia-tesla-t4,count=4 \
+    --accelerator=type=nvidia-tesla-t4,count=$GPUS \
     --zone=us-west1-b \
     --num-nodes=2 \
     --project=gunny-multi-instance-dev
 
-# INSERT POD AND SERVICE DEPLOYMENT
+helm install --set numGPUs=$GPUS gwinf ./gw-triton
 
 gcloud container node-pools delete big-t4-pool \
     --cluster=gw-triton-dev \
